20689f124311c6da0dbb679e60266ca68e1d1351
michaeldsmith <37905569+michaeldsmith@users.noreply.github.com>
2022-07-03 08:13:45 -0700

    add valgrind options --show-leak-kinds=all --track-origins=yes

--- unittest/scripts/run_valgrind.sh
+++ unittest/scripts/run_valgrind.sh
@@ -7,11 +7,11 @@ set -u
 SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
 cd ./unittest/IlmCtl/
-valgrind -s --error-exitcode=1 --leak-check=full --track-origins=yes ./IlmCtlTest 
+valgrind -s --error-exitcode=1 --leak-check=full --track-origins=yes --show-leak-kinds=all ./IlmCtlTest 
 cd ../IlmCtlMath/
-valgrind -s --error-exitcode=1 --leak-check=full ./IlmCtlMathTest 
+valgrind -s --error-exitcode=1 --leak-check=full --track-origins=yes --show-leak-kinds=all ./IlmCtlMathTest 
 cd ../IlmImfCtl/
-valgrind -s --error-exitcode=1 --leak-check=full ./IlmImfCtlTest
+valgrind -s --error-exitcode=1 --leak-check=full --track-origins=yes --show-leak-kinds=all ./IlmImfCtlTest
 
 # go back to initial path
 cd $SCRIPTPATH
