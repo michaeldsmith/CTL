08d6db2d2322652151e0e0f3acd8943110d0ee91
michaeldsmith <37905569+michaeldsmith@users.noreply.github.com>
2022-07-10 00:09:33 -0700

    check valgrind exit codes

--- unittest/scripts/run_valgrind.sh
+++ unittest/scripts/run_valgrind.sh
@@ -7,11 +7,26 @@ set -u
 SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
 cd ./unittest/IlmCtl/
-valgrind -s --error-exitcode=1 --leak-check=full --track-origins=yes --show-leak-kinds=all ./IlmCtlTest 
+valgrind -s --error-exitcode=1 --leak-check=full --track-origins=yes --show-leak-kinds=all ./IlmCtlTest
+test1_status=$? 
+
 cd ../IlmCtlMath/
 valgrind -s --error-exitcode=1 --leak-check=full --track-origins=yes --show-leak-kinds=all ./IlmCtlMathTest 
+test_02_status=$? 
+
 cd ../IlmImfCtl/
 valgrind -s --error-exitcode=1 --leak-check=full --track-origins=yes --show-leak-kinds=all ./IlmImfCtlTest
+test_03_status=$? 
 
 # go back to initial path
 cd $SCRIPTPATH
+
+# return valgrind exit codes
+if [ test_01_status -eq 0 ] && [ test_02_status -eq 0 ] && [ test_03_status -eq 0 ]
+then
+  echo "Success: valgrind detected no errors"
+  exit 0
+else
+  echo "Failure: valgrind detected errors"
+  exit 1
+fi
