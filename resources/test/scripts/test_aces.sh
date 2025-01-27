85043a4cd13be71abb96e01fa32d36a18be31bbc
Michael Smith <miksmith@attglobal.net>
2025-01-27 00:05:02 -0800

    progress running aces test

--- resources/test/scripts/test_aces.sh
+++ resources/test/scripts/test_aces.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 set -x
-set -v
+#set -v
 set -u 
 #set -e
 
@@ -16,9 +16,9 @@ CTLBASE=/usr/src/aces-dev/transforms/ctl
 export CTL_MODULE_PATH=$CTLBASE/csc/ADX/:$CTLBASE/idt/vendorSupplied/sony/:$CTLBASE/lib/:$CTLBASE/lmt/:$CTLBASE/odt/rec709/:$CTLBASE/rrt/:$CTLBASE/utilities/
 
 # camera to ACES
-ctlrender -force -compression NONE -ctl $CTLBASE/csc/ADX/ACEScsc.Academy.ADX10_to_ACES.ctl -global_param1 aIn 1.0 -format aces $INPUT_IMAGE_DIR/camera/DigitalLAD.2048x1556.dpx $OUTPUT_IMAGE_DIR/ACES_DigitalLAD.2048x1556.exr
-ctlrender -force -compression NONE -ctl $CTLBASE/idt/vendorSupplied/sony/IDT.Sony.SLog1_SGamut_10i.ctl -global_param1 aIn 1.0 -format aces $INPUT_IMAGE_DIR/camera/SonyF35.StillLife.dpx $OUTPUT_IMAGE_DIR/ACES_SonyF35.StillLife.exr
-ctlrender -force -compression NONE -ctl $CTLBASE/utilities/ACESutil.Unity.ctl -format aces $INPUT_IMAGE_DIR/camera/syntheticChart.01.exr $OUTPUT_IMAGE_DIR/ACES_syntheticChart.01.exr
+ctlrender -force -compression NONE -ctl $CTLBASE/csc/ADX/ACEScsc.Academy.ADX10_to_ACES.ctl -global_param1 aIn 1.0 -format exr16 $INPUT_IMAGE_DIR/camera/DigitalLAD.2048x1556.dpx $OUTPUT_IMAGE_DIR/ACES_DigitalLAD.2048x1556.exr
+ctlrender -force -compression NONE -ctl $CTLBASE/idt/vendorSupplied/sony/IDT.Sony.SLog1_SGamut_10i.ctl -global_param1 aIn 1.0 -format exr16 $INPUT_IMAGE_DIR/camera/SonyF35.StillLife.dpx $OUTPUT_IMAGE_DIR/ACES_SonyF35.StillLife.exr
+ctlrender -force -compression NONE -ctl $CTLBASE/utilities/ACESutil.Unity.ctl -global_param1 aIn 1.0 -format exr16 $INPUT_IMAGE_DIR/camera/syntheticChart.01.exr $OUTPUT_IMAGE_DIR/ACES_syntheticChart.01.exr
 
 # ACES to OCES
 ctlrender -force -compression NONE -ctl $CTLBASE/rrt/RRT.ctl -format exr16 $INPUT_IMAGE_DIR/ACES/SonyF35.StillLife.exr $OUTPUT_IMAGE_DIR/OCES_SonyF35.StillLife.exr
@@ -30,7 +30,7 @@ ctlrender -force -compression NONE -ctl $CTLBASE/odt/rec709/ODT.Academy.Rec709_1
 
 # concatenated ctl rendering
 ctlrender -force -compression NONE -ctl $CTLBASE/idt/vendorSupplied/sony/IDT.Sony.SLog1_SGamut_10i.ctl -global_param1 aIn 1.0 -ctl $CTLBASE/rrt/RRT.ctl -ctl $CTLBASE/odt/rec709/ODT.Academy.Rec709_100nits_dim.ctl -format tiff16 $INPUT_IMAGE_DIR/camera/SonyF35.StillLife.dpx $OUTPUT_IMAGE_DIR/ACESOCESODT_SonyF35.StillLife_ODT.Academy.Rec709_100nits_dim.tiff 
-ctlrender -force -compression NONE -ctl $CTLBASE/utilities/ACESutil.Unity.ctl -ctl $CTLBASE/rrt/RRT.ctl -ctl $CTLBASE/odt/rec709/ODT.Academy.Rec709_100nits_dim.ctl -format tiff16 $INPUT_IMAGE_DIR/camera/syntheticChart.01.exr $OUTPUT_IMAGE_DIR/ACESOCESODT_syntheticChart.01_ODT.Academy.Rec709_100nits_dim.tiff
+ctlrender -force -compression NONE -global_param1 aIn 1.0 -ctl $CTLBASE/utilities/ACESutil.Unity.ctl -ctl $CTLBASE/rrt/RRT.ctl -ctl $CTLBASE/odt/rec709/ODT.Academy.Rec709_100nits_dim.ctl -format tiff16 $INPUT_IMAGE_DIR/camera/syntheticChart.01.exr $OUTPUT_IMAGE_DIR/ACESOCESODT_syntheticChart.01_ODT.Academy.Rec709_100nits_dim.tiff
 
 # copy reference images
 cp $INPUT_IMAGE_DIR/ACES/DigitalLAD.2048x1556.exr $OUTPUT_IMAGE_DIR/ACESref_DigitalLAD.2048x1556.exr
@@ -72,6 +72,8 @@ test_08_label="ACESOCESODT_SonyF35.StillLife_ODT.Academy.Rec709_100nits_dim.tiff
 compare -verbose -metric PAE $OUTPUT_IMAGE_DIR/ACESOCESODT_syntheticChart.01_ODT.Academy.Rec709_100nits_dim.tiff $INPUT_IMAGE_DIR/ODT/syntheticChart.01_ODT.Academy.Rec709_100nits_dim.tiff null: 2>&1
 test_09_status=$? 
 test_09_label="ACESOCESODT_syntheticChart.01_ODT.Academy.Rec709_100nits_dim.tiff"
+
+set +x
 
 if [ $test_01_status -eq 0 ] && [ $test_02_status -eq 0 ] && [ $test_03_status -eq 0 ] && [ $test_04_status -eq 0 ] && [ $test_05_status -eq 0 ] && [ $test_06_status -eq 0 ] && [ $test_07_status -eq 0 ] && [ $test_08_status -eq 0 ] && [ $test_09_status -eq 0 ] && [ $test_10_status -eq 0 ]
 then
