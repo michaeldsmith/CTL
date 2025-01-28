54b8218942dd35e4f4cf626ade811140e89b9373
Michael Smith <miksmith@attglobal.net>
2025-01-28 14:42:25 -0800

    improve readability of ACES test script

--- resources/test/scripts/test_aces.sh
+++ resources/test/scripts/test_aces.sh
@@ -32,113 +32,144 @@ ctlrender -force -compression NONE -ctl $CTLBASE/odt/rec709/ODT.Academy.Rec709_1
 ctlrender -force -compression NONE -ctl $CTLBASE/idt/vendorSupplied/sony/IDT.Sony.SLog1_SGamut_10i.ctl -global_param1 aIn 1.0 -ctl $CTLBASE/rrt/RRT.ctl -ctl $CTLBASE/odt/rec709/ODT.Academy.Rec709_100nits_dim.ctl -format tiff16 $INPUT_IMAGE_DIR/camera/SonyF35.StillLife.dpx $OUTPUT_IMAGE_DIR/ACESOCESODT_SonyF35.StillLife_ODT.Academy.Rec709_100nits_dim.tiff 
 ctlrender -force -compression NONE -global_param1 aIn 1.0 -ctl $CTLBASE/utilities/ACESutil.Unity.ctl -ctl $CTLBASE/rrt/RRT.ctl -ctl $CTLBASE/odt/rec709/ODT.Academy.Rec709_100nits_dim.ctl -format tiff16 $INPUT_IMAGE_DIR/camera/syntheticChart.01.exr $OUTPUT_IMAGE_DIR/ACESOCESODT_syntheticChart.01_ODT.Academy.Rec709_100nits_dim.tiff
 
-# copy reference images
-cp $INPUT_IMAGE_DIR/ACES/DigitalLAD.2048x1556.exr $OUTPUT_IMAGE_DIR/ACESref_DigitalLAD.2048x1556.exr
-cp $INPUT_IMAGE_DIR/ACES/SonyF35.StillLife.exr $OUTPUT_IMAGE_DIR/ACESref_SonyF35.StillLife.exr
-cp $INPUT_IMAGE_DIR/ACES/syntheticChart.01.exr $OUTPUT_IMAGE_DIR/ACESref_syntheticChart.01.exr
-cp $INPUT_IMAGE_DIR/OCES/SonyF35.StillLife.exr $OUTPUT_IMAGE_DIR/OCESref_SonyF35.StillLife.01.exr
-cp $INPUT_IMAGE_DIR/OCES/syntheticChart.01.exr $OUTPUT_IMAGE_DIR/OCESref_syntheticChart.01.exr
-cp $INPUT_IMAGE_DIR/ODT/SonyF35.StillLife_ODT.Academy.Rec709_100nits_dim.tiff $OUTPUT_IMAGE_DIR/ODTref_SonyF35.StillLife_ODT.Academy.Rec709_100nits_dim.tiff
-cp $INPUT_IMAGE_DIR/ODT/syntheticChart.01_ODT.Academy.Rec709_100nits_dim.tiff $OUTPUT_IMAGE_DIR/ODTref_syntheticChart.01_ODT.Academy.Rec709_100nits_dim.tiff
+# copy reference images to output directory to simplify debugging and comparison
+cp $INPUT_IMAGE_DIR/ACES/DigitalLAD.2048x1556.exr $OUTPUT_IMAGE_DIR/ACESreference_ACES_DigitalLAD.2048x1556.exr
+cp $INPUT_IMAGE_DIR/ACES/SonyF35.StillLife.exr $OUTPUT_IMAGE_DIR/ACESreference_SonyF35.StillLife.exr
+cp $INPUT_IMAGE_DIR/ACES/syntheticChart.01.exr $OUTPUT_IMAGE_DIR/ACESreference_syntheticChart.01.exr
+cp $INPUT_IMAGE_DIR/OCES/SonyF35.StillLife.exr $OUTPUT_IMAGE_DIR/OCESreference_SonyF35.StillLife.01.exr
+cp $INPUT_IMAGE_DIR/OCES/syntheticChart.01.exr $OUTPUT_IMAGE_DIR/OCESreference_syntheticChart.01.exr
+cp $INPUT_IMAGE_DIR/ODT/SonyF35.StillLife_ODT.Academy.Rec709_100nits_dim.tiff $OUTPUT_IMAGE_DIR/ODTreference_SonyF35.StillLife_ODT.Academy.Rec709_100nits_dim.tiff
+cp $INPUT_IMAGE_DIR/ODT/syntheticChart.01_ODT.Academy.Rec709_100nits_dim.tiff $OUTPUT_IMAGE_DIR/ODTreference_syntheticChart.01_ODT.Academy.Rec709_100nits_dim.tiff
 
 # compare images
-compare -verbose -metric PAE $OUTPUT_IMAGE_DIR/ACES_DigitalLAD.2048x1556.exr $INPUT_IMAGE_DIR/ACES/DigitalLAD.2048x1556.exr null: 2>&1
-test_01_status=$? 
+METRIC_VALUE="PAE"
+#DIFFERENCE_IMAGE_FILENAME="null:" # use null: to prevent creation of output difference image file
+ 
+COMPUTED_IMAGE_FILENAME="${OUTPUT_IMAGE_DIR}/ACES_DigitalLAD.2048x1556.exr"
+REFERENCE_IMAGE_FILENAME="${INPUT_IMAGE_DIR}/ACES/DigitalLAD.2048x1556.exr"
+DIFFERENCE_IMAGE_FILENAME="${OUTPUT_IMAGE_DIR}/difference_image_ACES_DigitalLAD.2048x1556.exr"
+compare -verbose -metric ${METRIC_VALUE} ${COMPUTED_IMAGE_FILENAME} ${REFERENCE_IMAGE_FILENAME} ${DIFFERENCE_IMAGE_FILENAME} 2>&1
+test_01_exitcode=$? 
 test_01_label="ACES_DigitalLAD.2048x1556.exr"
-compare -verbose -metric PAE $OUTPUT_IMAGE_DIR/ACES_SonyF35.StillLife.exr $INPUT_IMAGE_DIR/ACES/SonyF35.StillLife.exr null: 2>&1
-test_02_status=$? 
+
+COMPUTED_IMAGE_FILENAME="${OUTPUT_IMAGE_DIR}/ACES_SonyF35.StillLife.exr"
+REFERENCE_IMAGE_FILENAME="${INPUT_IMAGE_DIR}/ACES/SonyF35.StillLife.exr"
+#DIFFERENCE_IMAGE_FILENAME="${OUTPUT_IMAGE_DIR}/difference_image_ACES_SonyF35.StillLife.exr"
+DIFFERENCE_IMAGE_FILENAME="null:"
+compare -verbose -metric ${METRIC_VALUE} ${COMPUTED_IMAGE_FILENAME} ${REFERENCE_IMAGE_FILENAME} ${DIFFERENCE_IMAGE_FILENAME} 2>&1
+test_02_exitcode=$? 
 test_02_label="ACES_SonyF35.StillLife.exr"
-compare -verbose -metric PAE $OUTPUT_IMAGE_DIR/ACES_syntheticChart.01.exr $INPUT_IMAGE_DIR/ACES/syntheticChart.01.exr null: 2>&1
-test_03_status=$? 
+
+COMPUTED_IMAGE_FILENAME="${OUTPUT_IMAGE_DIR}/ACES_syntheticChart.01.exr"
+REFERENCE_IMAGE_FILENAME="${INPUT_IMAGE_DIR}/ACES/syntheticChart.01.exr"
+compare -verbose -metric $METRIC_VALUE ${COMPUTED_IMAGE_FILENAME} ${REFERENCE_IMAGE_FILENAME} ${DIFFERENCE_IMAGE_FILENAME} 2>&1
+test_03_exitcode=$? 
 test_03_label="ACES_syntheticChart.01.exr"
 
-compare -verbose -metric PAE $OUTPUT_IMAGE_DIR/OCES_SonyF35.StillLife.exr $INPUT_IMAGE_DIR/OCES/SonyF35.StillLife.exr null: 2>&1
-test_04_status=$? 
+COMPUTED_IMAGE_FILENAME="${OUTPUT_IMAGE_DIR}/OCES_SonyF35.StillLife.exr"
+REFERENCE_IMAGE_FILENAME="${INPUT_IMAGE_DIR}/OCES/SonyF35.StillLife.exr"
+compare -verbose -metric $METRIC_VALUE ${COMPUTED_IMAGE_FILENAME} ${REFERENCE_IMAGE_FILENAME} ${DIFFERENCE_IMAGE_FILENAME} 2>&1
+test_04_exitcode=$? 
 test_04_label="OCES_SonyF35.StillLife.exr"
-compare -verbose -metric PAE $OUTPUT_IMAGE_DIR/OCES_syntheticChart.01.exr $INPUT_IMAGE_DIR/OCES/syntheticChart.01.exr null: 2>&1
-test_05_status=$? 
+
+COMPUTED_IMAGE_FILENAME="${OUTPUT_IMAGE_DIR}/OCES_syntheticChart.01.exr"
+REFERENCE_IMAGE_FILENAME="${INPUT_IMAGE_DIR}/OCES/syntheticChart.01.exr"
+compare -verbose -metric $METRIC_VALUE ${COMPUTED_IMAGE_FILENAME} ${REFERENCE_IMAGE_FILENAME} ${DIFFERENCE_IMAGE_FILENAME} 2>&1
+test_05_exitcode=$? 
 test_05_label="OCES_syntheticChart.01.exr"
 
-compare -verbose -metric PAE $OUTPUT_IMAGE_DIR/ODT_SonyF35.StillLife_ODT.Academy.Rec709_100nits_dim.tiff $INPUT_IMAGE_DIR/ODT/SonyF35.StillLife_ODT.Academy.Rec709_100nits_dim.tiff null: 2>&1
-test_06_status=$? 
+COMPUTED_IMAGE_FILENAME="${OUTPUT_IMAGE_DIR}/ODT_SonyF35.StillLife_ODT.Academy.Rec709_100nits_dim.tiff"
+REFERENCE_IMAGE_FILENAME="${INPUT_IMAGE_DIR}/ODT/SonyF35.StillLife_ODT.Academy.Rec709_100nits_dim.tiff"
+compare -verbose -metric $METRIC_VALUE ${COMPUTED_IMAGE_FILENAME} ${REFERENCE_IMAGE_FILENAME} ${DIFFERENCE_IMAGE_FILENAME} 2>&1
+test_06_exitcode=$? 
 test_06_label="ODT_SonyF35.StillLife_ODT.Academy.Rec709_100nits_dim.tiff"
-compare -verbose -metric PAE $OUTPUT_IMAGE_DIR/ODT_syntheticChart.01_ODT.Academy.Rec709_100nits_dim.tiff $INPUT_IMAGE_DIR/ODT/syntheticChart.01_ODT.Academy.Rec709_100nits_dim.tiff null: 2>&1
-test_07_status=$? 
+
+COMPUTED_IMAGE_FILENAME="${OUTPUT_IMAGE_DIR}/ODT_syntheticChart.01_ODT.Academy.Rec709_100nits_dim.tiff"
+REFERENCE_IMAGE_FILENAME="${INPUT_IMAGE_DIR}/ODT/syntheticChart.01_ODT.Academy.Rec709_100nits_dim.tiff"
+compare -verbose -metric $METRIC_VALUE ${COMPUTED_IMAGE_FILENAME} ${REFERENCE_IMAGE_FILENAME} ${DIFFERENCE_IMAGE_FILENAME} 2>&1
+test_07_exitcode=$? 
 test_07_label="ODT_syntheticChart.01_ODT.Academy.Rec709_100nits_dim.tiff"
 
-compare -verbose -metric PAE $OUTPUT_IMAGE_DIR/ACESOCESODT_SonyF35.StillLife_ODT.Academy.Rec709_100nits_dim.tiff $INPUT_IMAGE_DIR/ODT/SonyF35.StillLife_ODT.Academy.Rec709_100nits_dim.tiff null: 2>&1
-test_08_status=$? 
+COMPUTED_IMAGE_FILENAME="${OUTPUT_IMAGE_DIR}/ACESOCESODT_SonyF35.StillLife_ODT.Academy.Rec709_100nits_dim.tiff"
+REFERENCE_IMAGE_FILENAME="${INPUT_IMAGE_DIR}/ODT/SonyF35.StillLife_ODT.Academy.Rec709_100nits_dim.tiff"
+compare -verbose -metric $METRIC_VALUE ${COMPUTED_IMAGE_FILENAME} ${REFERENCE_IMAGE_FILENAME} ${DIFFERENCE_IMAGE_FILENAME} 2>&1
+test_08_exitcode=$? 
 test_08_label="ACESOCESODT_SonyF35.StillLife_ODT.Academy.Rec709_100nits_dim.tiff"
-compare -verbose -metric PAE $OUTPUT_IMAGE_DIR/ACESOCESODT_syntheticChart.01_ODT.Academy.Rec709_100nits_dim.tiff $INPUT_IMAGE_DIR/ODT/syntheticChart.01_ODT.Academy.Rec709_100nits_dim.tiff null: 2>&1
-test_09_status=$? 
+
+COMPUTED_IMAGE_FILENAME="${OUTPUT_IMAGE_DIR}/ACESOCESODT_syntheticChart.01_ODT.Academy.Rec709_100nits_dim.tiff"
+REFERENCE_IMAGE_FILENAME="${INPUT_IMAGE_DIR}/ODT/syntheticChart.01_ODT.Academy.Rec709_100nits_dim.tiff"
+compare -verbose -metric $METRIC_VALUE ${COMPUTED_IMAGE_FILENAME} ${REFERENCE_IMAGE_FILENAME} ${DIFFERENCE_IMAGE_FILENAME} 2>&1
+test_09_exitcode=$? 
 test_09_label="ACESOCESODT_syntheticChart.01_ODT.Academy.Rec709_100nits_dim.tiff"
 
 set +x
+echo ""
+echo "Test Summary:"
 
-if [ $test_01_status -eq 0 ] && [ $test_02_status -eq 0 ] && [ $test_03_status -eq 0 ] && [ $test_04_status -eq 0 ] && [ $test_05_status -eq 0 ] && [ $test_06_status -eq 0 ] && [ $test_07_status -eq 0 ] && [ $test_08_status -eq 0 ] && [ $test_09_status -eq 0 ] && [ $test_10_status -eq 0 ]
+if [ $test_01_exitcode -eq 0 ] && [ $test_02_exitcode -eq 0 ] && [ $test_03_exitcode -eq 0 ] && [ $test_04_exitcode -eq 0 ] && [ $test_05_exitcode -eq 0 ] && [ $test_06_exitcode -eq 0 ] && [ $test_07_exitcode -eq 0 ] && [ $test_08_exitcode -eq 0 ] && [ $test_09_exitcode -eq 0 ] && [ $test_10_exitcode -eq 0 ]
 then
-    echo "Success: ACES ctlrender output matches ACES reference files exactly"
+    echo "SUCCESS: ACES ctlrender output matches ACES reference files exactly"
     exit 0
 else
     echo "FAILURE: ACES ctlrender output does not match ACES reference files exactly"
 
-    if [ $test_01_status -ne 0 ]
+    if [ $test_01_exitcode -ne 0 ]
     then 
         echo "$test_01_label: imagemagick compare detected differences" 
     else 
         echo "$test_01_label: imagemagick compare no detected differences" 
     fi
 
-    if [ $test_02_status -ne 0 ]
+    if [ $test_02_exitcode -ne 0 ]
     then 
         echo "$test_02_label: imagemagick compare detected differences" 
     else 
         echo "$test_02_label: imagemagick compare no detected differences" 
     fi
 
-    if [ $test_03_status -ne 0 ]
+    if [ $test_03_exitcode -ne 0 ]
     then 
         echo "$test_03_label: imagemagick compare detected differences" 
     else 
         echo "$test_03_label: imagemagick compare no detected differences" 
     fi
 
-    if [ $test_04_status -ne 0 ]
+    if [ $test_04_exitcode -ne 0 ]
     then 
         echo "$test_04_label: imagemagick compare detected differences" 
     else 
         echo "$test_04_label: imagemagick compare no detected differences" 
     fi
 
-    if [ $test_05_status -ne 0 ]
+    if [ $test_05_exitcode -ne 0 ]
     then 
         echo "$test_05_label: imagemagick compare detected differences" 
     else 
         echo "$test_05_label: imagemagick compare no detected differences" 
     fi
 
-    if [ $test_06_status -ne 0 ]
+    if [ $test_06_exitcode -ne 0 ]
     then 
         echo "$test_06_label: imagemagick compare detected differences" 
     else 
         echo "$test_06_label: imagemagick compare no detected differences" 
     fi
 
-    if [ $test_07_status -ne 0 ]
+    if [ $test_07_exitcode -ne 0 ]
     then 
         echo "$test_07_label: imagemagick compare detected differences" 
     else 
         echo "$test_07_label: imagemagick compare no detected differences" 
     fi
 
-    if [ $test_08_status -ne 0 ]
+    if [ $test_08_exitcode -ne 0 ]
     then 
         echo "$test_08_label: imagemagick compare detected differences" 
     else 
         echo "$test_08_label: imagemagick compare no detected differences" 
     fi
 
-    if [ $test_09_status -ne 0 ]
+    if [ $test_09_exitcode -ne 0 ]
     then 
         echo "$test_09_label: imagemagick compare detected differences" 
     else 
